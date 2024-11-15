4dbc783097223228bac48809ac2e2ebe64953edc
howardsolutions <phungthinh0946519356@gmail.com>
2024-11-15 17:10:22 +0700

    Revert "adding into the widget"
    This reverts commit 0547c6563cb08f79d081fe6808337d57638019b8.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-            child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.blueAccent,
-              backgroundImage: NetworkImage(
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/640px-Donald_Trump_official_portrait.jpg'),
-            ),
-            Text(
-              'Donald Trump',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            )
-          ],
-        )),
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
-    );
-  }
+    ),
+  );
 }
