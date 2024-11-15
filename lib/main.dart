fdbd40d6caa8a493661c083b7b6c9f2e4ac9b1c8
howardsolutions <phungthinh0946519356@gmail.com>
2024-11-15 17:18:17 +0700

    feat: UI card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blueAccent,
+              backgroundImage: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/640px-Donald_Trump_official_portrait.jpg'),
+            ),
+            Text(
+              'Donald Trump',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
