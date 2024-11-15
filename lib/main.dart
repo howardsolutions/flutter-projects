41e1c1cdda94639f4bb524b90ce05203206983ce
howardsolutions <phungthinh0946519356@gmail.com>
2024-11-15 17:43:37 +0700

    feat: UI mi card

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blue,
         body: SafeArea(
             child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50,
@@ -25,6 +25,64 @@ class MyApp extends StatelessWidget {
             Text(
               'Donald Trump',
               style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'THE PRESIDENT',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            SizedBox(
+              height: 20,
+              width: 100,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            Card(
+              color: Colors.yellow,
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+44 123 456 789",
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.yellow,
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email_outlined,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "trump@gmail.com",
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         )),
